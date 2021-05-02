80dcd87640dc5c735c097b496cce0be0399284e0
praneetha <praneetha.kondam@gmail.com>
2021-05-02 17:58:40 +0530

    commiting component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component : DashboardComponent},
+  {path:"dash",component: DashboardComponent},
+  {path:"repo",component:RepositoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { FileslistComponent } from './fileslist/fileslist.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent,
+    FileslistComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
